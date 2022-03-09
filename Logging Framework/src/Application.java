bda765ae7ac03d28a8328f578588e2984c675523
Verma BS <101172023+VermaBs@users.noreply.github.com>
2022-03-09 16:18:49 +0530

    Add files via upload

---
+++ Logging Framework/src/Application.java
@@ -0,0 +1,13 @@
+import logger_module.Logger;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Logger logger = Logger.getLogger();
+	       //logger.info("this is an info");
+	       // logger.debug("this is a debug");
+	        logger.error("this is an error");
+	}
+
+}
